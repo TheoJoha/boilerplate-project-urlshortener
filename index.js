5f837a26715182de0aab69597d50a757a6c78da3
Theodor Johansson <t.j.u@telia.com>
2023-10-27 16:08:55 +0200

    Began implementing checking of url

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const dns = require('dns');
+const url = require('url'); 
+const lookupUrl = "https://stackoverflow.com/questions/41942690/removing-http-or-http-and-www/41942787";
+
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -17,6 +22,22 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+  const parsedLookupUrl = url.parse(lookupUrl);
+  console.log(parsedLookupUrl);
+  dns.lookup(parsedLookupUrl.hostname,(error,address,family)=>{
+    console.log(error,address,family)
+  });
+});
+
+app.post('/api/shorturl', function(req, res, next) {
+  const name = req.query.name;
+    res.redirect("/documents");
+  dns.lookup(host, cb) 
+
+  req.user = getTheUserSync();  // Hypothetical synchronous operation
+  next();
+}, function(req, res) {
+  res.send("original url", "short url" );
 });
 
 app.listen(port, function() {
